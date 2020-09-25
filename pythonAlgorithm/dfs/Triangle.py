3ab5cb374167e5b40b576070f705879c0dd163b2
zhangzt <zhangztsky@gmail.com>
2020-09-25 10:02:23 +0800

    highlevel

---
+++ pythonAlgorithm/dfs/Triangle.py
@@ -0,0 +1,9 @@
+class Solution:
+    """
+    @param triangle: a list of lists of integers
+    @return: An integer, minimum path sum
+    """
+
+    def minimumTotal(self, triangle):
+        # write your code here
+        pass
