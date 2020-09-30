1a7a18d7056658c81b7b1b11ae359733de7433cf
zhangzt <zhangztsky@gmail.com>
2020-09-30 09:15:18 +0800

    dfs

--- pythonAlgorithm/dfs/Surrounded Regions2.py
+++ pythonAlgorithm/dfs/Surrounded Regions2.py
@@ -25,11 +25,12 @@ class Solution:
                     # if self.bfs(board, i, j, hash): board[i][j] = 'X'
                     # print(res)
                     res.clear()  # TODO  每次递归前先清空 因为我的思路是 上下左右都为true的时候，才更新
+        print(hash)
         return board
 
     def dfs(self, board, x, y, res, mark, hash):
-        directionX = [-1, 1, 0, 0]
-        directionY = [0, 0, -1, 1]
+        directionX = [-1, 0, 1, 0]
+        directionY = [0, -1, 0, 1]
         if (x <= 0 or y <= 0 or x >= len(board) - 1 or y >= len(board[0]) - 1) and board[x][y] == 'O': return False
         if x < 0 or y < 0 or x >= len(board) or y >= len(board[0]): return False
         if board[x][y] == 'X': return True
@@ -42,10 +43,9 @@ class Solution:
                 isok = self.dfs(board, directionX[i] + x, directionY[i] + y, res, mark, hash)
                 if board[x][y] == 'O': mark[x][y] = True
                 res.append(isok)  # TODO 标记了已访问导致其他的递归不能进去，那就在这个递归结束后回去掉标记  好
-        hash[x][y] = False not in res
-        return hash[x][y]
-        # else:
-        #     return True  # 如下图在（1,1）这一轮递归中，他已经访问过（1,1）点了，然后他在（2,1）点又想访问（1,1），那么我们不让他访问，让他直接return true，return None也行，就是不能return fasle
+            hash[x][y] = False not in res
+            return hash[x][y]
+        # 如下图在（1,1）这一轮递归中，他已经访问过（1,1）点了，然后他在（2,1）点又想访问（1,1），那么我们不让他访问，让他直接return true，return None也行，就是不能return fasle
         # 做标记主要是房主重复访问导致的死循环
 
     def bfs(self, grid, x, y, hash):
@@ -54,8 +54,8 @@ class Solution:
         if hash[x][y] != -1:
             return hash[x][y]  # 一招记忆化 时间复杂度从 3^n 到 m*n
 
-        directionX = [-1, 1, 0, 0]
-        directionY = [0, 0, -1, 1]
+        directionX = [-1, 0, 1, 0]
+        directionY = [0, 1, 0, -1]
         q = Queue()
         q.put(Coordinate(x, y))
         s = set()
@@ -84,11 +84,13 @@ class Solution:
         if coor.y < 0 or coor.y >= len(grid[0]): return True
         return False
 
-
+'''
+["XXXXX","XXXXX","XXOOX","XXOXX","XXOXX"]
+'''
 s = Solution()
 ss = s.surroundedRegions([['X', 'X', 'X', 'X', 'X'],
                           ['X', 'X', 'X', 'X', 'X'],
-                          ['X', 'X', 'O', 'O', 'X'],
+                          ['X', 'O', 'O', 'O', 'X'],
                           ['X', 'X', 'O', 'X', 'X'],
                           ['X', 'X', 'O', 'X', 'X'],
                           ])
@@ -118,6 +120,7 @@ def f(arr):
 
 
 f(a)
+res[11][19] = 'X'
 for i in res:
     print(i)
 print("*********")
