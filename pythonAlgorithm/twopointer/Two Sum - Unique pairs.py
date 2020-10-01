25615350e41c9723a6cbb1a7bc0ccca5d9274a18
zhangzt <zhangztsky@gmail.com>
2020-10-01 16:15:26 +0800

    prefixsum

--- pythonAlgorithm/twopointer/Two Sum - Unique pairs.py
+++ pythonAlgorithm/twopointer/Two Sum - Unique pairs.py
@@ -5,6 +5,20 @@ class Solution:
     @return: An integer
 
     给一整数数组, 找到数组中有多少组 不同的元素对 有相同的和, 且和为给出的 target 值, 返回对数.
+除了下面和那个hashmap，还可以；
+    def twoSum(self, numbers, target):
+        # write your code here
+
+        numbers = sorted(numbers)
+        i, j = 0, len(numbers) - 1
+        while i < j:
+            if target == numbers[i] + numbers[j]:
+                return [i, j]  # todo 此时i++，j-- 但是要保证 前后两个数不一样，为了去重   如1，1，2，3，6，6， target 7
+            if target > numbers[i] + numbers[j]:
+                i += 1
+            if target < numbers[i] + numbers[j]:
+                j -= 1
+        return -1
 
 
     这个题  不能先去重 
@@ -17,7 +31,7 @@ class Solution:
         hashmap = {}
         for i in range(len(numbers)):
             if target - numbers[i] in hashmap:
-                if (numbers[i], target - numbers[i]) not in s: s.add((target - numbers[i], numbers[i]))
+                if (numbers[i], target - numbers[i]) not in s: s.add((target - numbers[i], numbers[i]))#todo if (numbers[i], target - numbers[i]) 防止出现(100,7),(7,100)
             hashmap.update({numbers[i]: i})
         return len(s)
 
