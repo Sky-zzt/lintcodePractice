3420e9f351b1e8d202f6e2f29d05353c80f84d4f
zhangzt <zhangztsky@gmail.com>
2020-10-01 09:30:38 +0800

    add 2p

--- pythonAlgorithm/twopointer/Window Sum.py
+++ pythonAlgorithm/twopointer/Window Sum.py
@@ -20,8 +20,23 @@ class Solution:
             i += 1
             j += 1
         return res
+    # todo 优化上面 线程一个窗口后在累加的行为 -->改为 窗口移动一下，sum 减掉左边一个值，在加右边一个值   整体On
 
+    def winsum(self, nums, k):
+        i = 0
+        j = i + k - 1
+        sum = 0
+        res = []
+        for k in range(i, j + 1):
+            sum += nums[k]
+        while j < len(nums):
+            res.append(sum)
+            sum-=nums[i]
+            i += 1
+            j += 1
+            if j<len(nums):sum+=nums[j] # 判空 注意
+        return res
 
 a = []
 s = Solution()
-print(s.winSum(a, 9007))
+print(s.winsum([1,2,7,7,2], 3))
