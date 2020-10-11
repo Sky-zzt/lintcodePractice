4a85cce3d2ca6833030c170568316044f9b2ebed
zhangzt <zhangztsky@gmail.com>
2020-10-11 11:57:46 +0800

    数据结构调整，包的划分更加细致

--- pythonAlgorithm/datastrcture/Top K Frequent Words.py
+++ pythonAlgorithm/datastrcture/Top K Frequent Words.py
@@ -1,17 +1,59 @@
+import heapq
+
+from heapq import heappush, heappop
+from collections import defaultdict
+import collections
+
+
+class Node(object):
+    def __init__(self, int, str):
+        self.freq = int
+        self.word = str
+
+    # 自定义less than comparator
+    # freq不同的话正常比较
+    # freq相同时，看word, 词典序小的排在前面
+    def __lt__(self, other):
+        if self.freq != other.freq:
+            return self.freq < other.freq
+
+        return self.word > other.word
+
+
 class Solution:
     """
     @param words: an array of string
     @param k: An integer
     @return: an array of string
-    给一个单词列表，求出这个列表中出现频次最高的K个单词。
     """
 
     def topKFrequentWords(self, words, k):
         # write your code here
-        pass
+        if not words or k <= 0:
+            return []
+
+        map = collections.defaultdict(int)
+        ans = []
+
+        for word in words:
+            map[word] += 1
+
+            heap = []
+
+        for word, freq in map.items():
+            heapq.heappush(heap, Node(freq, word))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        while heap:
+            ans.append(heapq.heappop(heap).word)
+        ans.reverse()
+
+        return ans
 
 
-map={4:2,2:3}
+map = {4: 2, 2: 3}
 
-map=sorted(map.items(),key=lambda kv:(kv[0],kv[1]))
+map = sorted(map.items(), key=lambda kv: (kv[0], kv[1]))
 print()
--- pythonAlgorithm/datastrcture/Top k Largest Numbers II.py
+++ pythonAlgorithm/datastrcture/Top k Largest Numbers II.py
@@ -1,6 +1,17 @@
 import heapq
 
-
+'''
+实现一个数据结构，提供下面两个接口
+1.add(number) 添加一个元素
+2.topk() 返回前K大的数
+    前K大 不是第K大
+    用minheap 而不是maxheap。
+    维持一个大小为K的minheap ，然后来一个数和最小值比较，比最小值大就弹出最先值加入新来的， 这个heap就是topk
+    
+    那么maxheap为啥不行呢，首先如果是maxheap肯定不能维持一个大小为k的heap，加入这个maxheap是5，3，1，，K==3，那么新来的数如果是100
+    我没法弹出
+    [1，3，5，100，4，5，12]
+'''
 class Solution:
 
     # @param {int} k an integer
