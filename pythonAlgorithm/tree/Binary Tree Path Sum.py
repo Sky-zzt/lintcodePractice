fb203dc3a052fc49f76f797663e19bbf04342b4f
zhangzt <zhangztsky@gmail.com>
2020-10-13 01:20:01 +0800

    tree

--- pythonAlgorithm/tree/Binary Tree Path Sum.py
+++ pythonAlgorithm/tree/Binary Tree Path Sum.py
@@ -87,7 +87,29 @@ class Solution:
             sum -= root.right.val
             path.remove(path[-1])
 
-
+    '''
+    class Solution(object):
+        def pathSum(self, root, sum):
+            """
+            :type root: TreeNode
+            :type sum: int
+            :rtype: List[List[int]]
+            """
+            res, path = [], []
+    
+            def recur(root, tar,sum):
+                if not root: return
+                path.append(root.val)
+                sum += root.val
+                if tar == sum and not root.left and not root.right:
+                    res.append(list(path))
+                recur(root.left, tar,sum)
+                recur(root.right, tar,sum)
+                path.pop()
+    
+            recur(root, sum,0)
+            return res
+    '''
 
         '''
         
