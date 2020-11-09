fd8d5756f9d9d747a479c52fad07d52d1b2949e8
zhangzt_glb <zhangztsky@gmil.com>
2020-11-09 17:50:50 +0800

    rs   inerview

--- pythonAlgorithm/binarysearch/ClassicalBinarySearch.py
+++ pythonAlgorithm/binarysearch/ClassicalBinarySearch.py
@@ -11,30 +11,31 @@ class Solution:
 
     def binarySearch(self, nums, target, start, end):
         mid = (start + end) // 2
-        while start+1<end: #todo <=才行
+        while start + 1 < end:  # todo <=才行
             if nums[mid] == target: return mid
             if nums[mid] < target:
-                return  self.binarySearch(nums, target, mid + 1, end)  #todo 必须加1 ，不然，死循环 你输入一个[2],4试试
+                return self.binarySearch(nums, target, mid + 1, end)  # todo 必须加1 ，不然，死循环 你输入一个[2],4试试
             if nums[mid] > target:
-                return  self.binarySearch(nums, target, start, mid - 1)
+                return self.binarySearch(nums, target, start, mid - 1)
         return -1
-    def bs(self,nums, target):
+
+    def bs(self, nums, target):
         # write your code here
-        if not nums:return -1
-        l=len(nums)
-        start=0
+        if not nums: return -1
+        l = len(nums)
+        start = 0
         # todo need to l-1  or line 35 will be wrong
-        end=l-1
-        while start+1<end:
-            mid=(start+end)//2
-            if nums[mid]==target:return mid
-            elif nums[mid]<target:start=mid
-            elif nums[mid]>target:end=mid
-        if nums[start]==target:
+        end = l - 1
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid
+            elif nums[mid] > target:
+                end = mid
+        if nums[start] == target:
             return start
-        if nums[end]==target:
+        if nums[end] == target:
             return end
         return -1
-
-
-
--- pythonAlgorithm/test/hash.py
+++ pythonAlgorithm/test/hash.py
@@ -1,4 +1,11 @@
-import hashlib
-
-bucket = hashlib.md5('100'.encode()).hexdigest()[:1]
-print(bucket)
+a = [1, 2, 3, 4, 5, 6, 7, 8]
+print(id(a))
+print(id(a[:]))
+for i in a[:]:
+    if i > 5:
+        pass
+    else:
+        a.remove(i)
+    print(a)
+print('-----------')
+print(id(a))
